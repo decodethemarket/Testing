8af8071e51fcf6e023cec065f3d6957efb71a041
decodethemarket <jjmoore1970@gmail.com>
2018-08-27 19:16:39 +0100

    stats added

--- py_my.py
+++ py_my.py
@@ -1,6 +1,6 @@
 import numpy as np
 import math 
-
+from statistics import mean, median
 x = np.array([1,2,3,4,5], dtype=int)
 y = np.array([12,23,12,20,18], dtype=int)
 
@@ -10,3 +10,11 @@ print('jonathan')
 
 z = round(1022.22221,6)
 print(z)
+
+listx = [1,0,1,1,10,1]
+print(sum(listx))
+print(max(listx))
+print(min(listx))
+
+print(mean(listx))
+print(median(listx))
