3808ec658136f88e0fc5870749b7287086de8cdc
decodethemarket <jjmoore1970@gmail.com>
2018-08-27 18:59:01 +0100

    python change

--- py_my.py
+++ py_my.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math 
 
 x = np.array([1,2,3,4,5], dtype=int)
 y = np.array([12,23,12,20,18], dtype=int)
@@ -6,3 +7,6 @@ y = np.array([12,23,12,20,18], dtype=int)
 print(x)
 print(y)
 print('jonathan')
+
+z = round(1022.22221,6)
+print(z)
