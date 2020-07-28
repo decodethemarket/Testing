a0d4cdf195be899da7176d23fd0faa678087e11b
Jonathan Moore <42414134+decodethemarket@users.noreply.github.com>
2020-07-28 23:19:15 +0100

    Update py_my.py
    Test one

--- py_my.py
+++ py_my.py
@@ -1,6 +1,6 @@
 import numpy as np
 import math 
-from statistics import mean, median
+from statistics import mean, median, stdev
 x = np.array([1,2,3,4,5], dtype=int)
 y = np.array([12,23,12,20,18], dtype=int)
 
@@ -12,9 +12,14 @@ z = round(1022.22221,6)
 print(z)
 
 listx = [1,0,1,1,10,1]
-print(sum(listx))
-print(max(listx))
-print(min(listx))
 
-print(mean(listx))
-print(median(listx))
+for x in listx:
+  print(x)
+print("sum: ", sum(listx))
+print("max: ", max(listx))
+print("min: ", min(listx))
+
+print("mean: ", mean(listx))
+print("median: ", median(listx))
+print("standard deviation :", stdev(listx))
+print("End")
